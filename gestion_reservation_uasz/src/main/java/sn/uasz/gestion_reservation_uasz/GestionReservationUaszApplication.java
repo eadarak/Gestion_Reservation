07f37a2e86bb32b0441abdc2903ef5e93a28f79f
eadarak <elabdoudrame2001@gmail.com>
2024-03-21 20:29:53 +0000

    Initialisation du projet en backend

---
+++ gestion_reservation_uasz/src/main/java/sn/uasz/gestion_reservation_uasz/GestionReservationUaszApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.gestion_reservation_uasz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionReservationUaszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionReservationUaszApplication.class, args);
+	}
+
+}
---
+++ gestion_reservation_uasz/src/test/java/sn/uasz/gestion_reservation_uasz/GestionReservationUaszApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.gestion_reservation_uasz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionReservationUaszApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
